032c298c78f552ee3c1d9fd82f82c1c6962d18ce
Semghh <1832187999@qq.com>
2024-02-13 17:24:24 +0800

    +

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -7,4 +7,3 @@ export const userRouter = new KoaRouter({ prefix: "/user" });
 // 注册controller（暂时我没发现有好的方法可以自动注册。。先用着吧
 userRouter.get("/login", userController.login); //Path    Query
 userRouter.post("/register", userController.register); //Path    Query
-userRouter.get("/getById",  userController.register); //Path    Query
